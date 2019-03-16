60d1f5d3318985c0e385201ae6f75cb002f1b03a
narcisabadea <narcy611@yahoo.com>
2019-03-16 15:54:42 +0200

    added router

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,26 @@
   <v-app>
     <v-toolbar dark color="primary">
       <v-toolbar-side-icon></v-toolbar-side-icon>
-
       <v-toolbar-title class="white--text">Smile Sharing</v-toolbar-title>
-
       <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>search</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>apps</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>refresh</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>more_vert</v-icon>
-      </v-btn>
+       <v-menu offset-y>
+        <v-btn
+          flat
+          slot="activator">
+          <v-icon left>account_circle</v-icon>Cont
+        </v-btn>
+        <v-list>
+          <v-list-tile>
+            <router-link to="/Profile" tag="li" style="cursor:pointer">
+              <v-list-tile-title>Detalii cont</v-list-tile-title>
+            </router-link>
+          </v-list-tile>
+          <v-list-tile>
+              <v-list-tile-title style="cursor:pointer">Iesire cont</v-list-tile-title>
+          </v-list-tile>
+          <v-divider></v-divider>
+        </v-list>
+      </v-menu>
     </v-toolbar>
   </v-app>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
+import App from './App'
+import router from '@/router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  render: h => h(App)
+});
