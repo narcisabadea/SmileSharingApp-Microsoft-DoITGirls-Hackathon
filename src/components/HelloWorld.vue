41f36730d35502022a5a165a8b36b8a9a3f69427
Gheorghe Madalina Eleonora <madalinaeleonora.gheorghe@gmail.com>
2019-03-16 16:51:42 +0200

    added buttons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,22 @@
 <template>
-<v-container>
-  Test
-</v-container>
+  <v-container>
+    <!-- <router-link to="/CarOffer" style="cursor:pointer; color: white;"> -->
+    <v-btn
+      flat
+      to="CarOffer"
+      slot="activator">
+     Car Offer
+    </v-btn>
+    <!-- </router-link>
+     <router-link to="/Home" style="cursor:pointer; color: white;"> -->
+    <v-btn
+      flat
+      to="CarSearch"
+      slot="activator">
+     Search for a car
+    </v-btn>
+     <!-- </router-link> -->
+  </v-container>
 </template>
 
 <script>
