d85be98d1d0b35de0ced619c34a666a4d9939b1f
madalinaeleonorag <madalinaeleonora.gheorghe@gmail.com>
2019-03-16 21:29:33 +0200

    added locality going and leave filters

--- src/components/CarSearch.vue
+++ src/components/CarSearch.vue
@@ -2,9 +2,23 @@
   <v-container>
     <v-layout>
       <v-flex xs12>
+        <v-flex xs4 sm4>
+          <v-autocomplete
+            :items="localityLeaveFilter"
+            label="Locality leaving"
+            v-model="selectedLocalityLeave">
+          </v-autocomplete>
+        </v-flex>
+        <v-flex xs4 sm4>
+          <v-autocomplete
+            :items="localityGoingFilter"
+            label="Locality leaving"
+            v-model="selectedlocalityGoing">
+          </v-autocomplete>
+        </v-flex>
         <v-card-text>
           <v-list three-line id="culoare">
-            <v-list-tile v-for="(item, index) in items" :key="index">
+            <v-list-tile v-for="(item, index) in filteredItems" :key="index">
               <v-list-tile-content  >
                 <v-list-tile-title> <span style="opacity: 0.3;color:grey">From </span> {{item.localityLeave}} <span style="opacity: 0.3;color:grey">({{item.hourLeave}}:{{item.minLeave}}) to </span>{{item.localityGoing}}</v-list-tile-title>
                 <v-list-tile-sub-title class="text-truncate">Price: {{item.price}}RON</v-list-tile-sub-title>
@@ -24,11 +38,34 @@ export default {
   name: 'CarSearch',
   data () {
     return {
-      items: []
+      items: [],
+      localityGoing: [],
+      selectedLocalityLeave: 'All locations',
+      selectedlocalityGoing: 'All locations'
     }
   },
   computed: {
-    
+    filteredItems() {
+      return this.items.filter(item => {
+        return this.selectedLocalityLeave === 'All locations' ? true : item.localityLeave === this.selectedLocalityLeave
+      }).filter(item => {
+        return this.selectedlocalityGoing === 'All locations' ? true : item.localityGoing === this.selectedlocalityGoing
+      })
+    },
+    localityLeaveFilter() {
+      let localityLeaveFilterData = ['All locations']
+      this.items.forEach(item => {
+        localityLeaveFilterData.push(item.localityLeave)
+      })
+      return localityLeaveFilterData
+    },
+    localityGoingFilter() {
+      let localityGoingFilterData = ['All locations']
+      this.items.forEach(item => {
+        localityGoingFilterData.push(item.localityGoing)
+      })
+      return localityGoingFilterData
+    }
   },
   methods: {
     getData() {
